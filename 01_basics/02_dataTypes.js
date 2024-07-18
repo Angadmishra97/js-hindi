7114c01dd042b2cbfb97b754c8314775ec80f41d
Angadmishra97 <167812696+Angadmishra97@users.noreply.github.com>
2024-07-18 17:42:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) -> we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Angad");
+
+
+let name = "Angad"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => used to find uniqueness
+
+
+//object
+
+console.log(typeof undifined); //undifined
+console.log(typeof null); //object
