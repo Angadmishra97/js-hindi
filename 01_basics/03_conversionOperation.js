bfeef260cd3a1b23c92474b9c47ff27a6ef66171
Angadmishra97 <167812696+Angadmishra97@users.noreply.github.com>
2024-07-18 19:33:16 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "Angad"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "Angad" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
