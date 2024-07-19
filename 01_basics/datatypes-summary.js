e023a01b7cc0447620ba3399b425bdcc7a35a3d1
Angadmishra97 <167812696+Angadmishra97@users.noreply.github.com>
2024-07-19 10:21:03 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 //const bigNumber = 2344928579232568982n
 
@@ -31,4 +31,32 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof heros);
+//console.log(typeof heros);
+
+
+
+
+
+// ***************************************************************************************************
+
+// Memory are of two types => Stack (Primitive),  Heap (Non-Primitive)
+
+let myYoutubeName = "hiteshChoudharydotcom"
+
+let anothername = myYoutubeName
+anothername = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
