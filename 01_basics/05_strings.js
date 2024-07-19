5c47fc628362b24440cda7865c3d2804921ad04e
Angadmishra97 <167812696+Angadmishra97@users.noreply.github.com>
2024-07-19 13:48:09 +0000

    strings,js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Angad"
+const repoCount =50
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName =new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "    Angad    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Angad.com/Angad%20Mishra"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
