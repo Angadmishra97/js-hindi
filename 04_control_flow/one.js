fd4a3905ff5573040d352be4c3e253bc90f019aa
Angadmishra97 <167812696+Angadmishra97@users.noreply.github.com>
2024-07-21 13:47:42 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,34 @@
+//if
+
+//syntax
+// if(condition){
+
+// }
+
+// if(true){
+//     // code execute
+// }
+
+// if(false){
+//     // code not execute
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const temp = 41
+
+// if(temp === 50){
+//     console.log("less than 50");
+// }else {
+//     console.log("greater than 50");
+// }
+
+// const score = 200
+
+// if(score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
