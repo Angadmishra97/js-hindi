63242c93d338fd9e33c44a40ca7f3fdbb6093d0d
Angadmishra97 <167812696+Angadmishra97@users.noreply.github.com>
2024-07-21 07:50:45 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,4 +31,33 @@ function loginUserMessage(username){
 }
 
 //console.log(loginUserMessage("angad"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Angad",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 300, 400, 100]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
