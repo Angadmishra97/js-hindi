b31bc919d498938e034d1ebe0344d1c7f5d2b8f8
Angadmishra97 <167812696+Angadmishra97@users.noreply.github.com>
2024-07-23 04:58:14 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -32,3 +32,27 @@
 
 // console.log(`User power: ${power}`);
 
+
+// const balance = 1000
+// if(balance > 500) console.log("test"), console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2 == 3){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
